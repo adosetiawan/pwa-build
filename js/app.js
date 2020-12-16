dc0053c9673c4fbd01a044fcba47633235ef93c8
pth <43008513+adosetiawan@users.noreply.github.com>
2020-12-16 16:41:18 +0700

    remove sub folder

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
 //pwa
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/pwa/service-worker.js').then(function (registration) {
+    navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function (err) {
